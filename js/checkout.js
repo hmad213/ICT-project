07ec35cdc90e51cc37f12c74e24bfbdf9b861db6
hmad213 <142343785+hmad213@users.noreply.github.com>
2024-11-24 19:25:07 +0500

    Add checkout.html and about-us.html with css and js

---
+++ js/checkout.js
@@ -0,0 +1,61 @@
+let cart = JSON.parse(sessionStorage.getItem('cart')) || [];
+
+let update = false;
+let hr = false
+let total = 0
+let cartItems = document.getElementById("cart-items");
+for (let item in cart){
+    if(cart.hasOwnProperty(item)){
+        if(cart[item].amount > 0){
+            total += cart[item].amount * cart[item].price;
+            const itemDiv = document.createElement("div");
+            itemDiv.classList.add("item");
+            itemDiv.innerHTML = `<img src="../images/${cart[item].img}">\n
+                                <div class="details">\n
+                                    <h3>${item}</h3>\n
+                                    <span>Rs ${cart[item].price}</span>\n
+                                </div>\n
+                                <span>X ${cart[item].amount}</span>`;
+            cartItems.appendChild(itemDiv);
+            update = true;
+            cartItems.appendChild(document.createElement("hr"));
+        }
+    }
+}
+let totalDiv = document.getElementById("total");
+let tax = Math.round(total * 0.13);
+totalDiv.innerHTML = `<div>\n
+                        <span>Total:</span>\n
+                        <span>${total}</span>\n
+                        </div>\n
+                        <div>\n
+                        <span>Tax(13%):</span>\n
+                        <span>${tax}</span>\n
+                        </div>\n
+                        <div>\n
+                        <span>Delivery Fee:</span>\n
+                        <span>150</span>\n
+                        </div>
+                        <div>\n
+                        <span>Grand Total:</span>\n
+                        <span>${total+tax+150}</span>\n
+                        </div>\n
+                        <div id="checkout-button-div">\n
+                        <button id="checkout-button" onclick="save_cart()">Checkout</button>\n
+                        </div>`
+
+function select_option(option){
+    if (option === "cod"){
+        document.getElementById("cod").style.borderColor = "greenyellow";
+        document.getElementById("credit").style.borderColor = "red";
+    }
+    else{
+        document.getElementById("cod").style.borderColor = "red";
+        document.getElementById("credit").style.borderColor = "greenyellow";
+    }
+}
+
+function back_to_home(){
+    sessionStorage.setItem('cart', JSON.stringify(cart));
+    window.location.href = "../index.html"
+}
--- js/index.js
+++ js/index.js
@@ -1,51 +1,3 @@
-const items = {
-    "Deal 1":{
-        amount: 0,
-        img: "Deal-1.png",
-        price: 450
-    },
-    "Deal 2":{
-        amount: 0,
-        img: "Deal-2.png",
-        price: 750
-    },
-    "Deal 3":{
-        amount: 0,
-        img: "Deal-3.png",
-        price: 950
-    },
-    "Dal Bunkabab":{
-        amount: 0,
-        img: "Dal-Bunkabab.png",
-        price: 220
-    },
-    "Bunkabab with Egg":{
-        amount: 0,
-        img: "Bunkabab-with-Egg.png",
-        price: 250
-    },
-    "Chapli Burger":{
-        amount: 0,
-        img: "Chapli-Burger.png",
-        price: 250
-    },
-    "Fries":{
-        amount: 0,
-        img: "Fries.png",
-        price: 100
-    },
-    "Cold drink":{
-        amount: 0,
-        img: "Cold-Drink.png",
-        price: 100
-    },
-    "Loaded Fries":{
-        amount: 0,
-        img: "Loaded-Fries.png",
-        price: 200
-    }
-}
-
 let cart = document.getElementById("cart");
 let cartButton = document.getElementById("cart-button");
 let sidebar = document.getElementById("sidebar");
@@ -53,7 +5,63 @@ let sidebarButton = document.getElementById("sidebar-button");
 let overlay = document.getElementById("overlay");
 let cartItems = document.getElementById("cart-items");
 let emptyCart = document.getElementById("empty-cart");
-let usedCart = document.getElementById("used-cart")
+let usedCart = document.getElementById("used-cart");
+
+let items = JSON.parse(sessionStorage.getItem('cart')) || [];
+if (items == 0){
+    console.log("hi");
+    items = {
+        "Deal 1":{
+            amount: 0,
+            img: "Deal-1.png",
+            price: 450
+        },
+        "Deal 2":{
+            amount: 0,
+            img: "Deal-2.png",
+            price: 750
+        },
+        "Deal 3":{
+            amount: 0,
+            img: "Deal-3.png",
+            price: 950
+        },
+        "Dal Bunkabab":{
+            amount: 0,
+            img: "Dal-Bunkabab.png",
+            price: 220
+        },
+        "Bunkabab with Egg":{
+            amount: 0,
+            img: "Bunkabab-with-Egg.png",
+            price: 250
+        },
+        "Chapli Burger":{
+            amount: 0,
+            img: "Chapli-Burger.png",
+            price: 250
+        },
+        "Fries":{
+            amount: 0,
+            img: "Fries.png",
+            price: 100
+        },
+        "Cold drink":{
+            amount: 0,
+            img: "Cold-Drink.png",
+            price: 100
+        },
+        "Loaded Fries":{
+            amount: 0,
+            img: "Loaded-Fries.png",
+            price: 200
+        }
+    }
+}
+else{
+    update_cart()
+}
+console.log(items)
 
 cartButton.onclick = function(){
     cart.style.width = "500px";
@@ -69,6 +77,11 @@ overlay.onclick = function(){
     sidebar.style.width = "0px";
     cart.style.width = "0px";
     overlay.style.visibility = "hidden";
+}
+
+function save_cart(){
+    sessionStorage.setItem('cart', JSON.stringify(items));
+    window.location.href = "./pages/checkout.html"
 }
 
 function update_total(total){
@@ -90,8 +103,8 @@ function update_total(total){
                             <span>Grand Total:</span>\n
                             <span>${total+tax+150}</span>\n
                          </div>\n
-                         <div id="checkout-button">\n
-                            <button>Checkout</button>\n
+                         <div id="checkout-button-div">\n
+                            <button id="checkout-button" onclick="save_cart()">Checkout</button>\n
                          </div>`
 }
 
@@ -117,15 +130,14 @@ function update_cart(){
                                     </div>
                                     <span>X ${items[item].amount}</span>`;
                 cartItems.appendChild(itemDiv);
-                update = true
-                cartItems.appendChild(document.createElement("hr"))
+                update = true;
+                cartItems.appendChild(document.createElement("hr"));
             }
         }
     }
     if(update){
         usedCart.style.visibility = "visible";
         emptyCart.style.visibility = "hidden";
-
         update_total(total);
     }
     else{
